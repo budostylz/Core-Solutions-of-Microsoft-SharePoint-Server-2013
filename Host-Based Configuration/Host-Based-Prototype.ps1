1445dbf5e43a269b42155a206409f9801974cbee
budostylz <lewis.shaun1@outlook.com>
2016-08-07 17:48:04 -0400

    'commit'

--- Host-Based Configuration/Host-Based-Prototype.ps1
+++ Host-Based Configuration/Host-Based-Prototype.ps1
@@ -1,12 +1,12 @@
 ﻿
 #STEP 1 (CREATE WEB APPLICATION IF NEEDED AND IISRESET)
-<#
+
 New-SPWebApplication -Name 'Contoso Sites' -Port 19565 -ApplicationPool ContosoAppPool -ApplicationPoolAccount (Get-SPManagedAccount 'AD2012\SP_Farm') -AuthenticationProvider (New-SPAuthenticationProvider -UseWindowsIntegratedAuthentication)
 IISRESET /noforce 
-#>
+
 
 #STEP 2(CREATE HOST-BASED SITE COLLECTION)
-#New-SPSite 'http://portal.contoso.com' -HostHeaderWebApplication 'http://sp2013srv:19565/' -Name 'Portal' -Description 'Customer Root' -OwnerAlias 'AD2012.LOC\Administrator' -language 1033 -Template 'STS#0'
+New-SPSite 'http://portal.contoso.com' -HostHeaderWebApplication 'http://sp2013srv:19565/' -Name 'Portal' -Description 'Customer Root' -OwnerAlias 'AD2012.LOC\Administrator' -language 1033 -Template 'STS#0'
 
 
 
